8ab2774a4207361eb3a67d3d4d02949e7d572512
Anastasiia Lukina <anlukina24@gmail.com>
2020-09-02 20:12:25 +0300

    Updates

---
+++ precache-manifest.e9c85e544e42d1fcd9b8cb75a52a62f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93790e487fec584aa69656485105ecde",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19f7b04f67b4dcc568f4",
+    "url": "./static/css/main.d94fbf79.chunk.css"
+  },
+  {
+    "revision": "8daccb04a7f88300915b",
+    "url": "./static/js/2.754e2893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.754e2893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f7b04f67b4dcc568f4",
+    "url": "./static/js/main.19766c0a.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
